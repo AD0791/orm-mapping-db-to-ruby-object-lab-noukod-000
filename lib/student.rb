73932eaf5b215c3c6338f98d6f7f53e3b26f96cf
Alexandro Disla <alexandrodisla@hotmail.com>
2019-06-04 23:35:47 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -2,9 +2,9 @@ class Student
   attr_accessor :id, :name, :grade
 
   def self.new_from_db(row)
-    new_song = self.new  
-  new_song.id = row[0]
-  new_song.name =  row[1]
+    new_student = self.new  
+    new_student.id = row[0]
+    new_student.name =  row[1]
   new_song.length = row[2]
   new_song
   end
